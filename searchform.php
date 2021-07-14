681394a3610e6676248c631ce02e69b572fd09e8
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2021-07-14 09:25:08 +0300

    Кастомизирована форма поиска

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url( '/' ) ?>" >
+    <div class="input-group u8-input-group">
+        <input type="text" class="flex-fill search-form u8-input-search-form" value="<?php echo get_search_query() ?>" name="s" id="s" />
+        <button type="submit" class="btn btn-outline-secondary u8-btn-search" id="searchsubmit">Поиск</button>
+    </div>
+</form>
