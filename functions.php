ffc5b6dd8eb4251effa5cc34a4eda7afcb52cadf
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2021-07-14 09:48:09 +0300

    Подключены иконки Awesome

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ add_action( 'after_setup_theme', 'u8_sirius_setup' );
  
 function u8_sirius_scripts() {
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+	wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/81a8242a38.js');
     wp_enqueue_style( 'main_style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'u8_sirius_scripts' );
