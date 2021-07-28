705e5233fe3b0a349f57f856eeee6622df99d3d7
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2021-07-28 09:34:38 +0300

    Стилизация формы комментариев

--- comments.php
+++ comments.php
@@ -38,6 +38,34 @@ if ( post_password_required() )
  
     <?php endif; // have_comments() ?>
  
-    <?php comment_form(); ?>
- 
-</div><!-- #comments -->
+    <?php
+    $commenter = wp_get_current_commenter();
+    $req = get_option( 'require_name_email' ); 
+    $aria_req = ( $req ? " aria-required='true'" : '' );
+    $consent   = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
+    $comment_args = array(
+        'fields' => array(
+            'author' => '<div class="form-group comment-form-author">
+			<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label>
+			<input id="author" class="form-control" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />
+		    </div>',
+            'email'  => '<div class="form-group comment-form-email">
+                <label for="email">' . __( 'Email' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label>
+                <input id="email" class="form-control" name="email type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" aria-describedby="email-notes"' . $aria_req . ' />
+            </div>',
+            'cookies' => '<div class="form-check comment-form-cookies-consent">'.
+                sprintf( '<input id="wp-comment-cookies-consent" class="form-check-input" name="wp-comment-cookies-consent" type="checkbox" value="yes"%s />', $consent ) .'
+                <label for="wp-comment-cookies-consent" class="form-check-label">'. __( 'Сохранить мои данные в браузере для последующих комментариев.' ) .'</label>
+            </div>'
+        ),
+        'comment_field' => '<div class="form-group comment-form-comment">
+                <label for="comment">' . _x( 'Comment', 'noun' ) . '</label>
+                <textarea id="comment" name="comment" class="form-control" rows="8" aria-required="true" required="required"></textarea>
+            </div>',
+        'class_submit' => 'btn btn-primary u8-btn-primary',
+        'submit_field' => '<div class="form-submit">%1$s %2$s</div>',
+        'format' => 'html5'
+    );
+    comment_form($comment_args); 
+    ?>
+</div>
